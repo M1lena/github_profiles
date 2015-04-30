8b21b4b6d7de3ecb0b4f91ef30a6e62789a32f7d
M1lena <stoevami@gmail.com>
2015-04-30 11:38:45 +0100

    refactored tests

--- test/e2e/githubProfileFeature.js
+++ test/e2e/githubProfileFeature.js
@@ -1,18 +1,20 @@
 describe('Github Profile finder', function() {
-  it('has a title', function() {
-    browser.get('http://localhost:8080');
 
+  var searchBox = element(by.model('searchCtrl.searchTerm'))
+  var searchButton = element(by.className('btn'))
+
+  beforeEach(function() {
+    browser.get('http://localhost:8080');
+  });
+
+  it('has a title', function() {
     expect(browser.getTitle()).toEqual('Github user search');
   });
 
-  describe('GitHub profile finder', function(){
-    it('finds profiles', function (){
-      browser.get('http://localhost:8080');
-
-      element(by.model('searchCtrl.searchTerm')).sendKeys('spike01');
-      element(by.className('btn')).click();
-
-      expect(element(by.binding('user.login')).getText()).toEqual('spike01');
-    });
+  it('finds profiles', function (){
+    searchBox.sendKeys('spike01');
+    searchButton.click();
+    expect(element(by.binding('user.login')).getText()).toEqual('spike01');
   });
 });
+
