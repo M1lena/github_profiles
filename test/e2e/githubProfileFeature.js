37875efd17ea9da0bdd33edf5debccb826e3228b
Sara OC <saramoo@hotmail.com>
2015-04-30 11:30:47 +0100

    search returns expected results

--- test/e2e/githubProfileFeature.js
+++ test/e2e/githubProfileFeature.js
@@ -4,5 +4,15 @@ describe('Github Profile finder', function() {
 
     expect(browser.getTitle()).toEqual('Github user search');
   });
-})
 
+  describe('GitHub profile finder', function(){
+    it('finds profiles', function (){
+      browser.get('http://localhost:8080');
+
+      element(by.model('searchCtrl.searchTerm')).sendKeys('spike01');
+      element(by.className('btn')).click();
+
+      expect(element(by.binding('user.login')).getText()).toEqual('spike01');
+    });
+  });
+});
